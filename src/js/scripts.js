2e2def616a2b581306eb3145aef61005b1b6a113
linn88-ui <linnliu@bu.edu>
2024-04-26 18:45:51 -0400

    java

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,14 +6,12 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
-
+// Wheel Spin
 const wheel = document.querySelector('.wheel');
 const spinbtn = document.querySelector("#spin");
-const value = Math.ceil(Math.random()*1440)
-
+const value = Math.ceil(2+Math.random()*60)*45
 function pickdish() {
     wheel.style.transform = "rotate("+value+"deg)";
-    value += Math.ceil(Math.random()*1440);}
-
-
+    value += Math.ceil(2+Math.random()*60)*45;}
 spinbtn.addEventListener("click", pickdish);
+
