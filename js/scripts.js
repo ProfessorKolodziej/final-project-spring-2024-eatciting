1c553e841f275903ef0a9226c579cd161d200771
linn88-ui <linnliu@bu.edu>
2024-04-29 10:41:33 -0400

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,84 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
-
+// Wheel Spin
 const wheel = document.querySelector('.wheel');
 const spinbtn = document.querySelector("#spin");
-const value = Math.ceil(Math.random()*1440)
+const spinDegree = Math.ceil(2 + Math.random() * 60) * 45;
 
-function pickdish() {
-    wheel.style.transform = "rotate("+value+"deg)";
-    value += Math.ceil(Math.random()*1440);}
+function spinWheel() {
+	wheel.style.transform = "rotate(" + spinDegree + "deg)";
+	console.log(cuisineName, selectedDish);
+}
+spinbtn.addEventListener("click", spinWheel);
+
+// Cuisine Selection
+const cuisineID = spinDegree % 360 / 45;
+let cuisineName;
+if (cuisineID === 0) {
+	cuisineName = 'Chinese';
+	cuisineDish = ['Kung Pao Chicken', 'Sweet and Sour Pork', 'Mapo Tofu', 'Hot Pot', 'Peking Duck']
+} else if (cuisineID === 1) {
+	cuisineName = 'Vietnamese';
+	cuisineDish = ['Pho', 'Banh Mi', 'Goi Cuon', 'Bun Thit Nuong', 'Ca Kho To']
+} else if (cuisineID === 2) {
+	cuisineName = 'Korean';
+	cuisineDish = ['Bibimbap', 'Kimchi', 'Bulgogi', 'Japchae', 'Tteokbokki']
+} else if (cuisineID === 3) {
+	cuisineName = 'Italian';
+	cuisineDish = ['Pizza', 'Spaghetti Carbonara', 'Lasagna', 'Risotto', 'Tiramisu']
+} else if (cuisineID === 4) {
+	cuisineName = 'FastFood';
+	cuisineDish = ['Burger', 'Fried Chicken', 'Hot Dog', 'Sandwich', 'Fries']
+} else if (cuisineID === 5) {
+	cuisineName = 'French';
+	cuisineDish = ['Croissant', 'Ratatouille', 'Bouillabaisse', 'Coq au Vin', 'Crème Brûlée']
+} else if (cuisineID === 6) {
+	cuisineName = 'Mexican';
+	cuisineDish = ['Tacos', 'Burrito', 'Guacamole', 'Enchilada', 'Churros']
+} else if (cuisineID === 7) {
+	cuisineName = 'Japanese';
+	cuisineDish = ['Sushi', 'Ramen', 'Tempura', 'Udon', 'Miso Soup']
+} else {
+	cuisineName = 'Unknown';
+	cuisineDish = ['Unkown']
+}
+
+//Box Display and Page Scroll
+const showBox = document.querySelector('.gifthide');
+function showGift() {
+	showBox.classList.toggle('gifthide')
+	showBox.classList.toggle('gift')
+	if (window.innerWidth >= 834) {
+		window.scroll({
+			top: 400,
+			left: 0,
+			behavior: 'smooth'
+		});
+	}
+
+};
+wheel.addEventListener("transitionend", showGift);
+
+// Box Popout & Wheel Hide
+const showDishbtn = document.querySelector(".gifthide")
+const showPlate = document.querySelector(".platehide")
+const hideWheel = document.querySelector(".wheel")
+const selectedDish = cuisineDish[Math.floor(Math.random() * cuisineDish.length)];
+const orderDish = document.getElementById('order').textContent = 'Order: ' + selectedDish;
+
+function showDish1() {
+	showPlate.classList.toggle('plate')
+	showPlate.classList.toggle('platehide')
+}
+function showDish2() {
+	hideWheel.classList.toggle('wheelhide')
+	hideWheel.classList.toggle('wheel')
+}
+async function showDish() {
+	await showDish1();
+	showDish2();
+}
+showDishbtn.addEventListener("click", showDish)
 
 
-spinbtn.addEventListener("click", pickdish);
